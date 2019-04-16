f0413ae21b8897bc0cd1c82c3f9bc6aa035ede9e
pablodiegoss <pablodiegoss@hotmail.com>
2019-04-16 14:41:40 -0300

    My WIP stuff for assimp

--- triangulo VBO/game/assets/shaders/fragment.glsl
+++ triangulo VBO/game/assets/shaders/fragment.glsl
@@ -1,18 +1,11 @@
 #version 330 core
 
 in vec3 v_color;
-in vec2 v_text_coord;
 
-uniform sampler2D ourTexture;
-
-out vec4 fragColor;
-
+out vec4 FragColor;
     
 
 void main()
 {
-   // FragColor = vec4(01.0f,0,1.0f,1.0f) ;
-  // fragColor = vec4(v_color,1.0);
-  fragColor = texture(ourTexture, v_text_coord)*vec4(v_color,0.30);
-
+    FragColor = vec4(v_color,1.0);
 }
--- triangulo VBO/game/src/main.cpp
+++ triangulo VBO/game/src/main.cpp
@@ -1,26 +1,25 @@
-#include <iostream>
-#include <fstream>
 #include <cmath>
-
+#include <fstream>
+#include <iostream>
+// Order matters
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-#define STB_IMAGE_IMPLEMENTATION
-#include <stb/stb_image.h>
-
 #include "shader.hpp"
 
+/* assimp include files. These three are usually needed. */
+#include <assimp/Importer.hpp>
+#include <assimp/postprocess.h>
+#include <assimp/scene.h>
 
 using namespace std;
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
     glViewport(0, 0, width, height);
 }
 
-void processInput(GLFWwindow *window)
-{
-    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+void processInput(GLFWwindow *window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 }
 
@@ -33,11 +32,10 @@ int main() {
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     // Mac OS only
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Playground", NULL, NULL);
-    if (window == NULL)
-    {
+    GLFWwindow *window = glfwCreateWindow(800, 600, "Playground", NULL, NULL);
+    if (window == NULL) {
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
@@ -45,108 +43,106 @@ int main() {
 
     glfwMakeContextCurrent(window);
 
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
 
     int screenWidth = 800;
     int screenHeight = 600;
-  
+
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 
+    Assimp::Importer importer;
+    std::string path =
+        "/home/pablo/repositorioGit/ICG/triangulo VBO/build/game/OBJ.blend";
+    const aiScene *scene =
+        importer.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs);
+    
+    string directory = path.substr(0, path.find_last_of('/'));
 
-    float vertices[] = {
-       
-         0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 1.0f,
-        -0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,
-         0.0f,  0.5f, 0.0f, 0.0f, 1.0f, 0.0f
-    };
-    float texCoords[] = {
-        0.0f, 0.0f,  // lower-left corner  
-        1.0f, 0.0f,  // lower-right corner
-        0.5f, 1.0f   // top-center corner
-    };
-    unsigned int indices[] = {
-        0, 1, 2
-    };
+    aiMesh *mesh = scene->mMeshes[];
+    fprintf(stderr, "ROLA");
 
+    float vertices[mesh->mNumVertices * 3];
+    for (unsigned int i = 0; i < mesh->mNumVertices; i = i + 3) {
+        vertices[i] = mesh->mVertices[i].x;
+        vertices[i + 1] = mesh->mVertices[i].y;
+        vertices[i + 2] = mesh->mVertices[i].z;
+    }
+    int totalFaces = 0;    
+    for (unsigned int i = 0; i < mesh->mNumFaces; i++) {
+        aiFace face = mesh->mFaces[i];
+        totalFaces += face.mNumIndices;
+    }
+    unsigned int indices[mesh->mNumFaces*totalFaces];
+    unsigned int k = 0;
+    for (unsigned int i = 0; i < mesh->mNumFaces; i++, k++) {
+        aiFace face = mesh->mFaces[i];
+        for (unsigned int j = 0; j < face.mNumIndices; j++, k++)
+            indices[k] = face.mIndices[j];
+    }
     unsigned int VBO;
     glGenBuffers(1, &VBO);
-
-    unsigned int VBO_textcoordinates;
-    glGenBuffers(1, &VBO_textcoordinates);
 
     unsigned int EBO;
     glGenBuffers(1, &EBO);
 
-
-    unsigned int TEX;
-    glGenTextures(1, &TEX);
-    stbi_set_flip_vertically_on_load(true); 
-    int width, height, nrChannels;
-    unsigned char *data = stbi_load("teste.jpg", &width, &height, &nrChannels, 0); 
- 
-
-    if (data)
-    {
-            glBindTexture(GL_TEXTURE_2D, TEX);
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-            glGenerateMipmap(GL_TEXTURE_2D);
-    }else{
-        std::cout << "Failed to load texture" << std::endl;
-    }   
-    stbi_image_free(data);
     Shader shader("game/assets/shaders/vertex.glsl",
                   "game/assets/shaders/fragment.glsl");
     shader.use();
 
-
-
+    // Declare group of Buffers
     unsigned int VAO;
     glGenVertexArrays(1, &VAO);
 
+    // Activate group of buffers
     glBindVertexArray(VAO);
 
+    // Activate buffer of data. "Use VBO"
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    // Malloc and Insert Data into buffer, "Insert into vbo..."
     glBufferData(GL_ARRAY_BUFFER, sizeof vertices, vertices, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *) 0);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *) (3*sizeof(float)));
 
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),
+                          (void *)0);
 
-    glBindBuffer(GL_ARRAY_BUFFER, VBO_textcoordinates);
-    glBufferData(GL_ARRAY_BUFFER, sizeof texCoords, texCoords, GL_STATIC_DRAW);
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
-    glActiveTexture(GL_TEXTURE0);
-     glBindTexture(GL_TEXTURE_2D, TEX);
-
+    
+    // Use EBO
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof indices, indices, GL_STATIC_DRAW);
-    
-    
+
+    // Copy indices to GPU
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof indices, indices,
+                 GL_STATIC_DRAW);
+
     glEnableVertexAttribArray(0);
     glEnableVertexAttribArray(1);
-    glEnableVertexAttribArray(2);  
 
+    // Deactivate VAO
     glBindVertexArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+    // Or manually deactivate all buffers
+    // glBindBuffer(GL_ARRAY_BUFFER, 0);
+    // glBindBuffer(GL_ARRAY_BUFFER, 1);
+    // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
     glEnable(GL_DEPTH_TEST);
 
-    while(!glfwWindowShouldClose(window))
-    {
+    while (!glfwWindowShouldClose(window)) {
         processInput(window);
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         shader.use();
-        glBindTexture(GL_TEXTURE_2D, TEX);
+
+        // Activate VAO
         glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawElements(GL_POINTS, 5, GL_UNSIGNED_INT, 0);
+        // glDrawArrays(GL_POINTS, 0, 3);
+
+        // Deactivate VAO
         glBindVertexArray(0);
 
         glfwSwapBuffers(window);
