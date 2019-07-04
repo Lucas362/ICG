b7ea62b0eba77c879b7e2c5386897a2c527a3179
lucas <lucasvitorifg@gmail.com>
2019-07-04 10:39:46 -0300

    Removing build folder

--- triangulo VBO/build/libs/glm/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/matheusbernardo/Documents/pablo/ICG/triangulo VBO/libs/glm
-# Build directory: /Users/matheusbernardo/Documents/pablo/ICG/triangulo VBO/build/libs/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("glm")
-subdirs("test")
